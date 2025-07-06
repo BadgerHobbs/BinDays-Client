e5c3d47ff5d2aa34fb6c6688692ac542a7a25714
BadgerHobbs <andyjriggs@gmail.com>
2025-07-07 00:57:13 +0100

    Added list of postcodes to tests instead of single.

--- integration_test/client_test.dart
+++ integration_test/client_test.dart
@@ -7,7 +7,29 @@ import 'package:bindays_client/client.dart';
 
 void main() {
   late Client client;
-  final String postcode = "BN14 9NS";
+  final List<String> postcodes = [
+    'BA2 2DL',
+    'B34 6BS',
+    'BS6 7SR',
+    'HP22 5XA',
+    'BD11 1JZ',
+    'L15 2HF',
+    'M15 6PN',
+    'NE46 3JR',
+    'OX4 3AT',
+    'PL3 6AG',
+    'CF72 9WR',
+    'SY3 7TB',
+    'B27 7AJ',
+    'BS16 7ES',
+    'PL8 2NG',
+    'SO15 5NR',
+    'TQ12 4EL',
+    'EX20 1ZF',
+    'WN7 2LG',
+    'BA13 3JR',
+    'BN14 9NS',
+  ];
 
   setUp(() {
     final baseUrl = Uri.parse("https://api.bindays.app");
@@ -20,27 +42,16 @@ void main() {
     expect(collectors.length, greaterThan(0));
   });
 
-  test('getCollector', () async {
-    var collector = await client.getCollector(postcode);
-    expect(collector.name, isNotNull);
-  });
+  for (final postcode in postcodes) {
+    test('getBinDays for postcode $postcode', () async {
+      var collector = await client.getCollector(postcode);
+      expect(collector.name, isNotNull);
 
-  test('getAddresses', () async {
-    var collector = await client.getCollector(postcode);
-    expect(collector.name, isNotNull);
+      var addresses = await client.getAddresses(collector, postcode);
+      expect(addresses, isNotEmpty);
 
-    var addresses = await client.getAddresses(collector, postcode);
-    expect(addresses, isNotEmpty);
-  });
-
-  test('getBinDays', () async {
-    var collector = await client.getCollector(postcode);
-    expect(collector.name, isNotNull);
-
-    var addresses = await client.getAddresses(collector, postcode);
-    expect(addresses, isNotEmpty);
-
-    var binDays = await client.getBinDays(collector, addresses.first);
-    expect(binDays, isNotEmpty);
-  });
+      var binDays = await client.getBinDays(collector, addresses.first);
+      expect(binDays, isNotEmpty);
+    });
+  }
 }
